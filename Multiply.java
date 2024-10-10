d2cece2bf05a58897d4a8ebaa4c29d380ba1003b
Mokshith <mokshi3953@gmail.com>
2024-10-10 16:38:59 +0530

    Added Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -1,9 +1,9 @@
-class  Sum{
+class Multiply{
     public static void main(String args[]){
         int a,b;
         b=10;
         a=5;
-        System.out.println(a+b);
+        System.out.println(a*b);
 // Added message in sum file
     }
 }
